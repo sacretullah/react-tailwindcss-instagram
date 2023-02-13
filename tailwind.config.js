7c076e0588574b7c909bd9b3f03c46b422f670a4
sacretullah <100204868+sacretullah@users.noreply.github.com>
2023-02-13 12:32:04 +0300

    a

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "logo-pattern":
+          "url(https://www.instagram.com/static/images/homepage/phones/home-phones-2x.png/cbc7174b4f05.png)",
+      },
+    },
+  },
+  plugins: [],
+};
