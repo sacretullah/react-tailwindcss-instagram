d412d0d9edd6320800fe059e1e30a7a66037b9c0
sacretullah <100204868+sacretullah@users.noreply.github.com>
2023-03-06 19:57:26 +0300

    a

--- src/App.js
+++ src/App.js
@@ -33,26 +33,52 @@ function App() {
           ref={ref}
         >
           <img
-            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-700 ease-linear"
             src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1-2x.png/cfd999368de3.png"
             alt=""
           />
           <img
-            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-700 ease-linear"
             src="https://www.instagram.com/static/images/homepage/screenshots/screenshot2-2x.png/80b8aebdea57.png"
             alt=""
           />
           <img
-            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-700 ease-linear"
             src="https://www.instagram.com/static/images/homepage/screenshots/screenshot3-2x.png/fe2540684ab2.png"
             alt=""
           />
           <img
-            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-700 ease-linear"
             src="https://www.instagram.com/static/images/homepage/screenshots/screenshot4-2x.png/8e9224a71939.png"
             alt=""
           />
         </div>
+      </div>
+      <div className="w-[350px] grid gap-y-3">
+        <div className="bg-white border px-[40px] pt-10 pb-6">
+          <a href="#" className="flex justify-center mb-8">
+            <img
+              className="h-[51px]"
+              src="https://www.instagram.com/static/images/web/logged_out_wordmark-2x.png/d2529dbef8ed.png"
+              alt=""
+            />
+          </a>
+        </div>
+        <form>
+          <label className="block relative">
+            <input
+              type="text"
+              className="bg-zinc-50 px-2 border rounded-sm outline-none focus:border-gray-300 w-full h[38px]"
+            ></input>
+          </label>
+          <button
+            type="submit"
+            disabled={true}
+            className="h-[38px] rounded bg-brand font-medium text-white text-sm disabled:opacity-50"
+          >
+            Log In
+          </button>
+        </form>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        brand: "#0095f6",
+      },
       backgroundImage: {
         "logo-pattern":
           "url(https://www.instagram.com/static/images/homepage/phones/home-phones-2x.png/cbc7174b4f05.png)",
